ff15b300d1bb5fa4da81a77d570d11ebdedcc77f
barrownicholas <75350582+barrownicholas@users.noreply.github.com>
2021-09-07 10:50:24 -0400

    Automatic commit from Bubble

---
+++ actions/ABX-8515g/server.js
@@ -0,0 +1,11 @@
+function(properties, context) {
+    const PDFLib = require('pdf-lib');
+    const fetch = require('node-fetch');
+    const url1 = properties.pdf_1;
+    const url2 = properties.pdf_2;
+    if url1.substring()
+    
+    
+    
+    
+}
